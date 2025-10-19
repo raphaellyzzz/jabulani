409e414649adf0ad696ce9b42dd78f5c0216d5ff
Jackson-Vitorio <foxyytkt@gmail.com>
2025-10-19 10:44:46 -0300

    adicao_hitboxes

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var pulo := false
-
+@onready var anim := $anim as AnimatedSprite2D
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -17,13 +17,18 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		$anim.scale.x = direction
+		anim.scale.x = direction
 		if !pulo:
-			$anim.play("correndo")  
+			anim.play("correndo")  
 	elif pulo:
-		$anim.play("salto")  
+		anim.play("salto")  
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$anim.play("idle-pre-ataque")
+		anim.play("idle-pre-ataque")
 
 	move_and_slide()
+
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("inimigos"):
+		anim.play("hitado")
