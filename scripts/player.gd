4e871ecd480ae027f54e9114ec8646e46d61bbde
Jackson-Vitorio <foxyytkt@gmail.com>
2025-10-18 21:56:53 -0300

    implementacao_litoral

---
+++ scripts/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var pulo := false
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		pulo = true
+	elif is_on_floor():
+		pulo = false
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		$anim.scale.x = direction
+		if !pulo:
+			$anim.play("correndo")  
+	elif pulo:
+		$anim.play("salto")  
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$anim.play("idle-pre-ataque")
+
+	move_and_slide()
---
+++ scripts/poluizao_adm.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+var pilar = [0,1,2]
+var pilarP1 = 0
+var pilarP2 = 0
+
+func _ready() -> void:
+	pass 
+
+func _process(_delta: float) -> void:
+	pass
+	
+
+func _on_timer_timeout() -> void:
+	pilar.shuffle()
+	pilarP1 = pilar[0]
+	pilarP2 = pilar[1]
+
+	if pilarP1 == 0 or pilarP2 == 0:
+		$"../pilar".start_flash()
+		await get_tree().create_timer(2.0).timeout
+		$"../pilar".move_platform()
+	if pilarP1 == 1 or pilarP2 == 1:
+		$"../pilar2".start_flash()
+		await get_tree().create_timer(2.0).timeout
+		$"../pilar2".move_platform()
+	if pilarP1 == 2 or pilarP2 == 2:
+		$"../pilar3".start_flash()
+		await get_tree().create_timer(2.0).timeout
+		$"../pilar3".move_platform()
+	await get_tree().create_timer(3.0).timeout
+	_on_timer_timeout()
+	
