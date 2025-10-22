e45cbb118919104cb4829bdfd6ea4e89b5832a7a
Jackson-Vitorio <foxyytkt@gmail.com>
2025-10-22 16:57:21 -0300

    Update atira_polu.gd

--- scripts/fase_litoral/Poluizao/atira_polu.gd
+++ scripts/fase_litoral/Poluizao/atira_polu.gd
@@ -4,5 +4,6 @@ func _process(_delta: float) -> void:
 	if vida == 0:
 		Global.olhos = true
 		Global.morte_adm = true
+		await get_tree().create_timer(0.75).timeout
 		Global.adm = true
 		queue_free()
